10e84d10cfe38ce74adf93a6b57c10e0784a6cc4
moraiskaua <moraiskaua03@gmail.com>
2024-02-27 11:35:47 -0300

    feat: create transactions card

--- src/view/components/PageLoader.tsx
+++ src/view/components/PageLoader.tsx
@@ -4,7 +4,7 @@ interface PageLoaderProps {}
 
 const PageLoader: React.FC<PageLoaderProps> = ({}) => {
   return (
-    <div className="h-full w-full fixed inset-0 bg-gray-50 grid place-items-center">
+    <div className="h-full w-full fixed z-50 inset-0 bg-gray-50 grid place-items-center">
       <Spinner />
     </div>
   );
--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -5,13 +5,16 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { MONTHS } from '../../../../../app/config/constants';
 import TransactionsSliderOptions from './TransactionsSliderOptions';
 import TransactionsSliderNavigation from './TransactionsSliderNavigation';
+import { formatCurrency } from '../../../../../app/utils/formatCurrency';
+import { CategoryIcon } from '../../../../components/icons/categories/CategoryIcon';
+import { cn } from '../../../../../app/utils/cn';
 
 interface TransactionsProps {}
 
 const Transactions: React.FC<TransactionsProps> = ({}) => {
   return (
-    <div className="bg-gray-100 rounded-2xl w-full h-full px-4 py-8 md:p-10">
-      <header className="">
+    <div className="bg-gray-100 rounded-2xl w-full h-full px-4 py-8 md:p-10 flex flex-col">
+      <header>
         <div className="flex justify-between items-center">
           <button className="flex items-center gap-2">
             <TransactionsIcon />
@@ -44,7 +47,25 @@ const Transactions: React.FC<TransactionsProps> = ({}) => {
           </Swiper>
         </div>
       </header>
-      <div className="mt-4">conteúdo</div>
+
+      <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+        <div className="bg-white p-4 rounded-2xl flex justify-between items-center gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="expense" />
+
+            <div>
+              <strong className="font-bold tracking-[-0.5px] block">
+                Almoço
+              </strong>
+              <span className="text-sm text-gray-600">27/02/2024</span>
+            </div>
+          </div>
+
+          <span className={cn('tracking-[-0.5px] font-medium')}>
+            {formatCurrency(782.9)}
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
